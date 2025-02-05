fe8e75db9b3f2e1ec813ce3d529e59d7e7892a41
Philip Yaw Boahene <boahenep22@gmail.com>
2025-02-05 14:09:24 -0800

    2nd day of typescript done

--- dist/ex04.js
+++ dist/ex04.js
@@ -1,17 +1,14 @@
+"use strict";
 // Exercise: Create a function `formatInput` that accepts either a string or number as input.
 // If it's a number, return it multiplied by 100. If it's a string, return it in uppercase.
-
-function formatInput(input: string | number): string | number {
+function formatInput(input) {
     if (typeof input === "number") {
         return input * 100;
     }
-
     if (typeof input === "string") {
         return input.toUpperCase();
     }
-
     return input; // Default return statement
 }
-
-console.log(formatInput(5));    // Expected output: 500
+console.log(formatInput(5)); // Expected output: 500
 console.log(formatInput("hello")); // Expected output: "HELLO"
